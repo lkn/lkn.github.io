5d7a09c11bec7262007a58743147f8824e5da9ef
Lynn <lkn@lkn-macbookpro.roam.corp.google.com>
2014-01-05 13:54:58 +1100

    .

--- static/js/init.js
+++ static/js/init.js
@@ -5,11 +5,6 @@ $('#mainNav').on('click touchstart', function(e) {
   e.preventDefault();
 })
 
-// clicking anywhere inside navigation or heading won’t close main menu popover
-$('#navigation section, #navigation .heading, #navigation .subheading, #navigation .menu_label').on('click touchstart', function(e){
-    e.stopPropagation();
-})
-
 // checks if navigation is enabled
 function showHideNav() {
   if ($('#mainNav').hasClass('expanded')) {
@@ -21,15 +16,15 @@ function showHideNav() {
 
 // shows the main menu popover
 function showNav() {
+  console.log("showNav");
   $('#mainNav').removeClass('invisible').addClass('expanded');
-  $('#fullWidthContainer').removeClass('notScrolling');
 }
 
 // hides the main menu popover
 function hideNav() {
+  console.log("hideNav");
   $('#mainNav').addClass('transition');
-  window.setTimeout(function(){$('body').removeClass('noScroll').removeAttr('style');}, 50); // allow animations to start before removing class (Firefox)
-  window.setTimeout(function(){$('#mainNav').removeClass('transition').removeClass('expanded');}, 200);
-  $('.menuIcon').blur(); // removes tab focus
-  $('#fullWidthContainer').addClass('notScrolling');
+  window.setTimeout(
+    function(){$('#mainNav').removeClass('transition').removeClass('expanded');},
+    100);
 }
